7bd4b3e57202cfcd9a8e6afddd1cb4570d49ef3d
root <root@ip-172-31-17-42.us-west-2.compute.internal>
2017-06-09 03:22:38 +0000

    adding app

---
+++ cookbooks/dotnet_app/files/sample_app.sh
@@ -0,0 +1,3 @@
+dotnet new console -o /tmp/hwapp
+cd /tmp/hwapp
+dotnet restore
--- cookbooks/dotnet_app/recipes/default.rb
+++ cookbooks/dotnet_app/recipes/default.rb
@@ -3,3 +3,12 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+
+cookbook_file '/tmp/sample_app.sh' do
+        source 'sample_app.sh'
+end
+
+execute 'dotnet' do
+        command 'sh /tmp/sample_app.sh'
+end
